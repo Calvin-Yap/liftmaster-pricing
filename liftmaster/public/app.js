0eda8e2d0397b14b02cb30fb20cb175495519837
Calvin Yap <calvin.yap@ryerson.ca>
2020-09-24 11:38:20 -0400

    config fix

--- liftmaster/public/app.js
+++ liftmaster/public/app.js
@@ -517,16 +517,3 @@ angular.module('app', ['ngRoute','firebase'])
     });
 
 
-
-    const config = {
-        apiKey: "AIzaSyCiA3baPou7irYi248UbTw5spkif3gSiFs",
-        authDomain: "liftmaster-3d0d7.firebaseapp.com",
-        databaseURL: "https://liftmaster-3d0d7.firebaseio.com",
-        projectId: "liftmaster-3d0d7",
-        storageBucket: "liftmaster-3d0d7.appspot.com",
-        messagingSenderId: "566258234825",
-        appId: "1:566258234825:web:f0220d0dcc4815b0796418",
-        measurementId: "G-XEM24L6NS9"
-      };
-
-    firebase.initializeApp(config);
